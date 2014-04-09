5901bf8cb081a88e3faca6e98f0d7cc7e56cd099
mimilus <mimilusubuntu@gmail.com>
2014-04-09 10:04:44 +0200

    grass footprint modified

--- init.lua
+++ init.lua
@@ -102,9 +102,11 @@ minetest.register_node("trail:water_source_swam", {
 })
 
 
-local mod_snow = minetest.get_modpath("snow")
+--local mod_snow = minetest.get_modpath("snow")
+--if mod_snow then
+--end
 
-if mod_building_blocks then 
+
 minetest.register_node("trail:snow_walked", {
 	tiles = {"trail_snow_footprint.png", "snow_snow.png"},
 	drawtype = "nodebox",
@@ -162,7 +164,7 @@ minetest.register_node("trail:snow_block_walked", {
 		footstep = {name="default_gravel_footstep", gain=0.3},
 	}),
 })
-end
+
 
 minetest.register_node("trail:wheat_walked", {
 	description = "Flattened Wheat",
